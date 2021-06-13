4e9ef67170853e53b32dfed16ee864c910df0fe6
alzdirty <zackyalbaehaki@gmail.com>
2021-06-14 05:04:31 +0700

    p

--- script.js
+++ script.js
@@ -1,51 +1,23 @@
-const merah = document.getElementsByTagName('input')[0];
-const hijau = document.getElementsByTagName('input')[1];
-const biru = document.getElementsByTagName('input')[2];
-
-
-// function ubahwarna(r, g, b) {
-//     document.body.style.backgroundColor = "rgb('+ r +','+ g +','+ b +')"
-// }
-
-// merah.addEventListener('input',function () {
-// const r = merah.value;
-// const g = hijau.value;
-// const b = biru.value;
-
-//     document.body.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')';
-// })
-// hijau.addEventListener('input',function () {
-// const r = merah.value;
-// const g = hijau.value;
-// const b = biru.value;
-
-//     document.body.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')';
-// })
-// biru.addEventListener('input',function () {
-// const r = merah.value;
-// const g = hijau.value;
-// const b = biru.value;
-
-//     document.body.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')';
-// })
-
 const pil = document.querySelectorAll('input');
 pil.forEach(function(p) {
     p.addEventListener('input', function() {
-        const a = p.className;
-        
-        // const g
-        // const b
-        
-        (a == 'merah')? 
-        r = p.value
-        // g = hijau
-        // b = biru
-        :false;
-        (a == 'hijau')? g = p.value :false;
-        (a == 'biru')? b = p.value :false;
-
-        console.log(r);
-        document.body.style.backgroundColor = 'rgb('+ r +',100,100)';
+        const r = pil[0].value;
+        const g = pil[1].value;
+        const b = pil[2].value;
+        document.body.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')';
     });
 });
+
+const bagiancode = document.querySelector('.codewarna');
+const trgb = document.querySelector('.codewarna .rgb');
+const prgb = document.querySelector('.codewarna .codergb');
+
+trgb.addEventListener('click', function () {
+        const r = pil[0].value;
+        const g = pil[1].value;
+        const b = pil[2].value;
+    const pbaru = document.createElement('p');
+    const textbaru = document.createTextNode(''+ r +','+ g +','+ b +'');
+    pbaru.appendChild(textbaru);
+    bagiancode.replaceChild(pbaru, prgb);
+})
