e7355a3c6d39834203c71265f4d2c301e16b2837
alzdirty <zackyalbaehaki@gmail.com>
2021-06-16 03:10:09 +0700

    master

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const prgb = document.querySelector('.codewarna .codergb');
+//merubah warna
 const pil = document.querySelectorAll('input');
 pil.forEach(function(p) {
     p.addEventListener('input', function() {
@@ -5,19 +7,6 @@ pil.forEach(function(p) {
         const g = pil[1].value;
         const b = pil[2].value;
         document.body.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')';
+         prgb.innerHTML = ''+ r +', '+ g +', '+ b +'';
     });
 });
-
-const bagiancode = document.querySelector('.codewarna');
-const trgb = document.querySelector('.codewarna .rgb');
-const prgb = document.querySelector('.codewarna .codergb');
-
-trgb.addEventListener('click', function () {
-        const r = pil[0].value;
-        const g = pil[1].value;
-        const b = pil[2].value;
-    const pbaru = document.createElement('p');
-    const textbaru = document.createTextNode(''+ r +','+ g +','+ b +'');
-    pbaru.appendChild(textbaru);
-    bagiancode.replaceChild(pbaru, prgb);
-})
