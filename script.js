778996be9d3b18309815767538292cb4b6efb68b
alzdirty <zackyalbaehaki@gmail.com>
2021-06-16 04:10:58 +0700

    master

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const prgb = document.querySelector('.codewarna .codergb');
+const phex = document.querySelector('.codewarna .codehex');
 //merubah warna
 const pil = document.querySelectorAll('input');
 pil.forEach(function(p) {
@@ -6,7 +7,17 @@ pil.forEach(function(p) {
         const r = pil[0].value;
         const g = pil[1].value;
         const b = pil[2].value;
+        const h = gethexa(r);
+        const e = gethexa(g);
+        const x = gethexa(b);
         document.body.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')';
          prgb.innerHTML = ''+ r +', '+ g +', '+ b +'';
+         phex.innerHTML = ''+ h +', '+ e +', '+ x +'';
     });
 });
+
+function gethexa(value) {
+    const hexa = parseInt(value).toString(16);
+    return hexa.length == 1 ? "0" + hexa : hexa;
+}
+
